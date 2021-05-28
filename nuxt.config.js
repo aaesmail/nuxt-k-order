92178fcded40fbe936b10519db3bd580580383b1
SaadBadr <saadbadr23@gmail.com>
2021-05-28 05:40:50 +0200

    adding auth check as a plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/api/init.js'],
+  plugins: ['~/api/init.js', '~/plugins/auth.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/auth.js
@@ -0,0 +1,11 @@
+import { setAuthToken } from '~/utils/auth'
+
+export default async function({ store, redirect }) {
+  const userToken = localStorage.getItem('user-token')
+  if (userToken) {
+    setAuthToken(userToken)
+    store.dispatch('auth/fetch_user')
+  }
+
+  store.dispatch('auth/fetch_admin')
+}
