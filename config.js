3564bee6337de81ca8f06c8318aea2296bea46ea
SaadBadr <saadbadr23@gmail.com>
2021-06-08 21:07:43 +0200

    adding production url

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-export const baseURL = 'http://127.0.0.1:8000/api/v1'
+export const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://127.0.0.1:8000/api/v1'
+    : 'https://korder.herokuapp.com/api/v1'
